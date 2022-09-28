598a88fee569f2672a9c43f55f8cbb6b4c1bfe74
Ranjith757 <ranjithmg757@gmail.com>
2022-09-28 12:41:33 +0530

    complete

---
+++ blue/equality.js
@@ -0,0 +1,8 @@
+let a=10;
+let b='10';
+let c="Rahul";
+let d="rahul";
+console.log(a==b);
+console.log(a===b);
+console.log(c==d);
+console.log(c===d);
--- blue/logical.js
+++
@@ -1,4 +0,0 @@
-let a=10;
-let b=20;
-let c=30;
-console.loga>b && b<c
