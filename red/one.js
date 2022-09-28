18df7270484d7cf851aef486ccafc6cad8be7a77
Ranjith757 <ranjithmg757@gmail.com>
2022-09-28 11:48:15 +0530

    okie

---
+++ red/one.js
@@ -0,0 +1,19 @@
+var a=10;
+var b=20;
+var c='thirty'
+var d=
+var =40.5;
+var f=true;
+var g="rahul"
+var h="gandhi"
+var i="false"
+
+console.log(a+b);
+ console.log(a+c);
+ console.log(a+d);
+ console.log(a+e);
+ console.log(a+f);
+ console.log(a+g);
+ console.log(a+i);
+ console.log(a+h);
+ console.log(a+d);
