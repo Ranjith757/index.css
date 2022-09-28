1afd45d6a67ce966bc0790b4f1deb700c42beb8d
Ranjith757 <ranjithmg757@gmail.com>
2022-09-28 12:10:00 +0530

    ok

--- blue/one.js
+++ blue/one.js
@@ -1,13 +1,12 @@
 var a=10;
 var b=20;
-var c='thirty'
-var d=
-var =40.5;
+var c='thirty';
+var d="red";
+var e=40.5;
 var f=true;
 var g="rahul"
 var h="gandhi"
 var i="false"
-
 console.log(a+b);
  console.log(a+c);
  console.log(a+d);
