0c7a3b6926a84364bad30edb4f8117891b521806
Ranjith757 <ranjithmg757@gmail.com>
2022-09-30 14:10:56 +0530

    done

---
+++ Assignment/even.js
@@ -0,0 +1,7 @@
+let i=3
+if(i%2==0){
+    console.log("even")
+}
+else{
+    console.log("odd")
+}
---
+++ Assignment/threedigit.js
@@ -0,0 +1,7 @@
+let i=757
+if(i>=100 && i<1000){
+    console.log("three digit number")
+}
+else{
+    console.log("not a three digit number")
+}
