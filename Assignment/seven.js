6336b03d97a2f460f111d74ed2925d7b5387a643
Ranjith757 <ranjithmg757@gmail.com>
2022-09-30 13:35:38 +0530

    ranji

---
+++ Assignment/seven.js
@@ -0,0 +1,6 @@
+let i=28
+if(i%7==0){
+    console.log("divisible")
+}else{
+    console.log("not divisible")
+}
---
+++ Assignment/three.js
@@ -0,0 +1,6 @@
+let i=30
+if (i%3 == 0){
+    console.log("multiple")
+}else{
+    console.log("not multiple")
+}
