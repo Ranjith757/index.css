fb9896e52cddbc8d215e81a5926d79896ba65490
Ranjith757 <ranjithmg757@gmail.com>
2022-10-10 13:14:40 +0530

    pop

---
+++ Assignment/array/arraymethod/let.js
@@ -0,0 +1,10 @@
+let arr=[10,20,30,40]
+console.log(arr)
+arr.push(50)
+console.log(arr)
+arr.pop()
+console.log(arr)
+arr.unshift(5)
+console.log(arr)
+arr.shift()
+console.log(arr)
---
+++ Assignment/array/arraymethod/slice.js
@@ -0,0 +1,2 @@
+let arr=[10,20,30,true,false,"Roman","Reigns",null]
+console.log(arr.slice (5,7))
